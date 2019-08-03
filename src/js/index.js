1a92b2058762bd23f459920ece5e31099a7ac01a
russelljohnwilliams <russellwilliamscc@gmail.com>
2019-08-03 17:21:42 +0100

    tick and untick checkbox persists on reload

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,6 @@ function getData(check) {
   .then(response => response.json())
   .then(data => {
     fadeElementsOnLoad(data);
-    displayTask(data);
   })
 }
 
@@ -18,27 +17,24 @@ function fadeElementsOnLoad(data) {
   var i = 0;
   var x = data.length
   var intr = setInterval(function() {
-    displayTask(i, data);
+    displayTask(data[i]);
     i++
-
     if (i == x ) clearInterval(intr);
-  }, 50)
+  }, 100)
 }
 
-function displayTask(i, data) {
-
-  var ul = document.getElementById('list');
-
-  // for(i = 0; i < data.length; i++){
+function displayTask(data) {
+    var ul = document.getElementById('list');
     var li = document.createElement('li');
     ul.appendChild(li);
-    var title = data[i].title;
-    var importance = data[i].importance;
+    var title = data.title;
+    var importance = data.importance;
+    li.setAttribute('id', data.id)
     setImportance(li, importance);
     adCheckbox(li);
     addTextToList(li, title);
     removeLoader()
-  // }
+  checkIsDone(li, data)
 }
 
 function addTextToList(li, title) {
@@ -50,7 +46,9 @@ function addTextToList(li, title) {
 function adCheckbox(li){  
   var button = li.appendChild(document.createElement('button'));
   button.innerHTML = "<div class='tick-box'>✓<div>"
+  button.setAttribute("onclick", tickUntickCheckbox);
   button.setAttribute("class", 'checkbox-button');
+  button.onclick = tickUntickCheckbox
 }
 
 function setImportance(text, imp) {
@@ -61,15 +59,46 @@ function removeLoader() {
   var load = document.getElementById('loader')
   if(load != null){
     load.style.opacity = 0
+    load.style.height = "80px"
+    load.style.width = "80px"
+    load.style.left = "calc(50% - 40px)"
     setTimeout(function(){ 
       load.remove()
-    }, 500);
-  
-
+    }, 600);
   }
 }
 
+function checkIsDone(li, data) {
+  if (data.isDone == 'true'){
+    li.classList.add('checked')
+  }
+}
 
+function tickUntickCheckbox() {
+  if (this.parentNode.classList.contains('checked') == false){
+    this.parentNode.classList.add('checked')
+    editIsDone(this, 'true')
+  }else{
+    this.parentNode.classList.remove('checked')
+    editIsDone(this, 'false')
+  }
+}
+
+function editIsDone(item, trueFalse){
+console.log("item", item.parentNode)
+  var patchData = {
+    method: 'PATCH', 
+    body: JSON.stringify({'isDone': trueFalse}),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  fetch( url+'/'+item.parentNode.id, patchData)
+  .then(response => response.json()
+    )
+  .catch(error => console.log('error is', error)
+    )
+}
 
 
 
